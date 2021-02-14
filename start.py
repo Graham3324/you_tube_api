d66e43d73e703cee462312b852697c423b5f8cf2
Graham3324 <Graham.Sayers@yahoo.com>
2021-02-14 13:40:56 -0500

    Second changes

--- start.py
+++ start.py
@@ -1,5 +1,12 @@
 import constants
 from googleapiclient.discovery import build
 
-youtube = build('youtube', 'v3', developerKey=constants.youtube_APIKEY)
+youtube = build("youtube", "v3", developerKey=constants.youtube_APIKEY)
 
+request = youtube.channels().list(part="statistics", forUsername="schefer5")
+
+response = request.execute()
+
+print(response)
+
+print("Got here")
